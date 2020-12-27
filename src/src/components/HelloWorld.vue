e49423ace58ad7c597d1167e4de921f543de8163
Rockhopper-Penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-12-27 08:38:59 +0000

    配列からJSONに変換後、配列に戻す

--- src/src/components/HelloWorld.vue
+++ src/src/components/HelloWorld.vue
@@ -1,15 +1,74 @@
 <template>
   <div class="hello">
-    <p>{{ penguin }}</p>
+    <div class="block">
+      <p>Array</p>
+      <div v-for="index in forms" :key="index">
+        <input type="text" v-model="array[index - 1]" @change="onChange" />
+        <span v-if="index == 1">
+          <button @click="addForms">+</button>
+        </span>
+        <span v-else>
+          <button @click="removeForms">-</button>
+        </span>
+      </div>
+    </div>
+    <div class="block">
+      <p>JSON</p>
+      <p>{{ toJson }}</p>
+    </div>
+    <div class="block">
+      <p>JSON to array</p>
+      <div v-for="index in toArray" :key="index">
+        <input type="text" :value="index" />
+      </div>
+    </div>
   </div>
 </template>
 
+<style>
+.hello {
+  width: 100%;
+}
+.block {
+  width: 80%;
+  margin: auto;
+  border: solid 1px black;
+  padding: 10px;
+}
+</style>
+
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
 
 @Component
 export default class HelloWorld extends Vue {
   penguin = "Rockhopper-Penguin";
+
+  // 配列
+  array = [];
+  // フォーム数
+  forms = 1;
+  // JSONに変換
+  toJson = JSON.stringify(this.array);
+  // JSONから配列に変換
+  toArray = JSON.parse(this.toJson);
+
+  addForms() {
+    this.forms++;
+  }
+
+  removeForms() {
+    this.forms--;
+  }
+
+  // @Watch("array")
+  onChange() {
+    console.log("配列の中身が変更！！！");
+    // JSONに変換
+    this.toJson = JSON.stringify(this.array);
+    // JSONから配列に変換
+    this.toArray = JSON.parse(this.toJson);
+  }
 }
 </script>
 
